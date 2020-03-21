d738cd7a8982176de54d34541d5973f094624c67
laiskaSiili <marc.folini@gmail.com>
2020-03-21 12:38:30 +0100

    Connect tabletennis app

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '22id05@3k*pw_iw4t&f*%8)0l#)xhwmwiblgb72pf_0#yul-*y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tabletennis',
 ]
 
 MIDDLEWARE = [
--- tabletennis/views.py
+++ tabletennis/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse('<h1>It works!</h1>')
