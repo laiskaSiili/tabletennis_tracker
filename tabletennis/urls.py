41b73b2b5ce4482c4aec3f4a128be5a959251ae0
Pedro Vieira <pedrompsvieira@gmail.com>
2020-03-21 16:05:42 +0100

    Add ApiNameAvailability view

--- tabletennis/urls.py
+++ tabletennis/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tabletennis.views import LandingPageView
+from tabletennis.views import LandingPageView, ApiNameAvailability
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing_page'),
+    path('api/v0/nameavailability', ApiNameAvailability.as_view(), name='api_name_availability'),
 ]
--- tabletennis/views.py
+++ tabletennis/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from tabletennis.models import Player
 
 class LandingPageView(View):
     def get(self, request):
         ctx = {}
         return render(request, 'tabletennis/landingpage.html', ctx)
+
+class ApiNameAvailability(View):
+    def get(self, request):
+
+        name = request.GET.get('name')
+        name = name.strip().lower()
+
+        exists = 1 if Player.objects.filter(name__iexact=name).exists() else 0
+
+        return JsonResponse({
+            'name': name,
+            'exists': exists
+        })
