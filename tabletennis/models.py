7bb55c242909db8d6b680ce37d122ae3ded07f2e
laiskaSiili <marc.folini@gmail.com>
2020-03-22 11:05:08 +0100

    improve manager doc

--- tabletennis/models.py
+++ tabletennis/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class PlayerManager(models.Manager):
+    """
+    Custom manager for Player model to make name lookup case insensitive
+    by replacing the name lookup argument in get() and filter() with name__iexact.
+    A little hacky, but it works and allows the modelform validation to detect name collisions
+    independent of case.
+    """
     def filter(self, **kwargs):
         if 'name' in kwargs:
             kwargs['name__iexact'] = kwargs['name']
