1200e57855d3b759d74be7000a9679153cf88709
laiskaSiili <marc.folini@gmail.com>
2020-05-04 04:18:42 +0200

    Add minimalistic Game admin.

--- tabletennis/admin.py
+++ tabletennis/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from tabletennis.models import Player
+from tabletennis.models import Player, Game
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
     pass
